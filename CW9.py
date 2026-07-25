d4590dc6e30d2bb66540125e6b9ba9bd2d37fd43
ShivanshVerma2007 <shiv199991@gmail.com>
2026-07-25 13:56:08 +0530

    All functions of numpy approx.

---
+++ CW9.py
@@ -0,0 +1,55 @@
+# import numpy as np
+# import pandas as pd
+# import matplotlib.pyplot as plt
+
+# #to remove the duplicate element form a list
+# l=[1,2,3,4,2,3]
+# l=list(set(l))
+# print(l)
+
+# #to access the tuple elements
+# t=(1,2,3,4,5,6)
+# print(t[0:7])
+
+# arr=np.array([1,2,3,4,5,6,7,8,9,10])
+# rand=np.random.randint(0,50,20)
+# print(arr)
+# print()
+# print(arr.shape)
+# print()
+# print(arr.ndim)
+# print()
+# print(arr.size)
+# print()
+# print(arr+10)
+# print()
+# print(arr/10)
+# print()
+# print(arr*10)
+# print()
+# print(arr%10)
+# print()
+# print(arr**2)
+# print()
+# print(np.mean(arr))
+# print()
+# print(np.std(arr))
+# print()
+# print(np.min(arr))
+# print()
+# print(np.abs(arr))
+# print()
+# print(np.max(arr))
+# print()
+# print(np.sin(arr))
+# print()
+# print(np.cos(arr))
+# print()
+# print(np.tan(arr))
+# print()
+# print(np.exp(arr))
+# print()
+# print(np.log(arr))
+
+# for i in range(0,6):
+#     print("* "*i)
